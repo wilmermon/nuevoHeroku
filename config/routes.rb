0b3e4ea134c74790ef8eb1f96d3997039cdb06d8
Germán A. Chica Duque <Gchicaduque@gmail.com>
2018-05-10 18:10:07 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  
+  token = ENV["HIREFIRE_TOKEN"] || "development"
+  get "/hirefire/#{token}/info", to: "hirefire#info"
 
 end
